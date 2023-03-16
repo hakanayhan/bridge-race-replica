5742f0d8bcfccc9ab878b259ef8fcda9c38b9fec
Hakan Ayhan <hakanayhnn@gmail.com>
2023-03-16 18:25:56 +0300

    +AI bricks controller to get them to a bridge

--- Assets/Scripts/AI/AIController.cs
+++ Assets/Scripts/AI/AIController.cs
@@ -6,7 +6,7 @@ using DG.Tweening;
 
 public class AIController : MonoBehaviour
 {
-    public enum Colors { Red, Green, Blue}
+    public enum Colors { Red, Green, Blue }
     [SerializeField] private Colors _selectColor;
 
     public GameObject targetBrickParent;
@@ -16,12 +16,12 @@ public class AIController : MonoBehaviour
     [SerializeField] private NavMeshAgent _agent;
     [SerializeField] private float _radius;
     [SerializeField] private Transform _backpackObject;
-    [SerializeField] private List<GameObject> _cubes = new List<GameObject>();
+    [SerializeField] private List<GameObject> _bricks = new List<GameObject>();
+    [SerializeField] private Transform[] _bridges;
     [SerializeField] private GameObject _prevObject;
     private Vector3 _targetTransform;
-   
-
     private bool _haveTarget = false;
+
     private void Start()
     {
         for (int i = 0; i < targetBrickParent.transform.childCount; i++)
@@ -40,32 +40,50 @@ public class AIController : MonoBehaviour
 
     void FindTarget()
     {
-        Collider[] hitColliders = Physics.OverlapSphere(transform.position, _radius);
-        List<Vector3> ourColors = new List<Vector3>();
-        for (int i = 0; i < hitColliders.Length; i++)
-        {
-            if (hitColliders[i].tag == _selectColor.ToString())
-            {
-                ourColors.Add(hitColliders[i].transform.position);
-            }
-        }
 
-        if (ourColors.Count > 0)
+        int randomN = Random.Range(0, 3);
+
+        if (randomN == 0 && _bricks.Count >= 5)
         {
-            _targetTransform = ourColors[0];
+            int randomRope = Random.Range(0, _bridges.Length);
+            List<Transform> ropesNonActiveChild = new List<Transform>();
+            foreach (Transform item in _bridges[randomRope])
+            {
+                if (!item.GetComponent<MeshRenderer>().enabled || item.GetComponent<MeshRenderer>().enabled && item.gameObject.tag != "Diz" + _selectColor.ToString())
+                {
+                    ropesNonActiveChild.Add(item);
+                }
+            }
+            _targetTransform = _bricks.Count > ropesNonActiveChild.Count ? ropesNonActiveChild[ropesNonActiveChild.Count - 1].position : ropesNonActiveChild[_bricks.Count].position;
         }
         else
         {
-            int random = Random.Range(0, targetBricks.Count);
-            _targetTransform = targetBricks[random].transform.position;
-        }
+            Collider[] hitColliders = Physics.OverlapSphere(transform.position, _radius);
+            List<Vector3> ourColors = new List<Vector3>();
+            for (int i = 0; i < hitColliders.Length; i++)
+            {
+                if (hitColliders[i].tag == _selectColor.ToString())
+                {
+                    ourColors.Add(hitColliders[i].transform.position);
+                }
+            }
 
-        _agent.SetDestination(_targetTransform);
-        if (!_animator.GetBool("running"))
-        {
-            _animator.SetBool("running", true);
+            if (ourColors.Count > 0)
+            {
+                _targetTransform = ourColors[0];
+            }
+            else
+            {
+                int random = Random.Range(0, targetBricks.Count);
+                _targetTransform = targetBricks[random].transform.position;
+            }
         }
-        _haveTarget = true;
+            _agent.SetDestination(_targetTransform);
+            if (!_animator.GetBool("running"))
+            {
+                _animator.SetBool("running", true);
+            }
+            _haveTarget = true;
     }
 
     private void OnTriggerEnter(Collider target)
@@ -81,10 +99,15 @@ public class AIController : MonoBehaviour
             target.transform.localRotation = new Quaternion(0,0.7071068f,0, 0.7071068f);
             target.transform.DOLocalMove(pos, 0.2f);
             _prevObject = target.gameObject;
-            _cubes.Add(target.gameObject);
+            _bricks.Add(target.gameObject);
             target.tag = "Untagged";
             targetBricks.Remove(target.gameObject);
             _haveTarget = false;
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, _radius);
     }
 }
